00563d177304cd42447a64d64c6f9de61ee7294f
JohanGov <att.johgo@gmail.com>
2012-11-07 21:25:50 +0100

    Added WebCrawler

---
+++ FinalBuilderMonitor.Test/MainVMTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FinalBuilderMonitor;
+using System.IO;
+
+namespace FinalBuilderMonitor.Test
+{
+    [TestFixture]
+    public class MainVMTests
+    {
+        [Test]
+        public void TestCrawlerTowardsGithubJohanGovers()
+        {
+            var html = new MainVM().GetPageContent("https://github.com/JohanGovers");
+
+            Assert.IsTrue(html.Contains("github.com"));
+        }
+    }
+}
--- FinalBuilderMonitor/MainVM.cs
+++ FinalBuilderMonitor/MainVM.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FinalBuilderMonitor
 {
-    class MainVM
+    public class MainVM
     {
+        public MainVM()
+        {
+            
+        }
+
+        public string GetPageContent(string url)
+        {
+            var webRequest = (HttpWebRequest)WebRequest.Create(url);
+            webRequest.Method = "GET";
+
+            Stream stream = webRequest.GetResponse().GetResponseStream();
+            StreamReader reader = new StreamReader(stream);
+            return reader.ReadToEnd();
+        }
     }
 }
