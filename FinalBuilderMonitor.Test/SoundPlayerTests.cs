f65e6c8e011c4c35753a9845abb99b31c7fda8c4
JohanGov <att.johgo@gmail.com>
2012-11-18 20:39:59 +0100

    Added tests

---
+++ FinalBuilderMonitor.Test/SoundPlayerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FinalBuilderMonitor.Test
+{
+    [TestFixture]
+    public class SoundPlayerTests
+    {
+        [Test]
+        [Ignore("Exploratory test.")]
+        public void PlayASound()
+        {
+            SoundPlayer player = new SoundPlayer(@"TestData\AddonComplete.wav");
+            player.Play();
+        }
+    }
+}
